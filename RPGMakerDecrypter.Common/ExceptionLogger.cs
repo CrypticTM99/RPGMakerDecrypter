7873c70cd68147a45b5e35a3edf48bfe3dff4b83
Cryptic <67986767+CrypticTM99@users.noreply.github.com>
2025-04-08 19:36:39 -0300

    Update ExceptionLogger.cs

--- RPGMakerDecrypter.Common/ExceptionLogger.cs
+++ RPGMakerDecrypter.Common/ExceptionLogger.cs
@@ -1,14 +1,30 @@
-﻿using System;
+using System;
 using System.IO;
+using System.Text;
 
 namespace RPGMakerDecrypter.Common
 {
+    /// <summary>
+    /// Utility class for logging exceptions to a temporary file.
+    /// </summary>
     public static class ExceptionLogger
     {
+        /// <summary>
+        /// Logs the provided exception to a uniquely named temporary log file.
+        /// </summary>
+        /// <param name="exception">The exception to log.</param>
+        /// <returns>The full path to the created log file.</returns>
         public static string LogException(Exception exception)
         {
-            var outputFilePath = Path.Combine(Path.GetTempPath(), $"RPGMakerDecrypter-{Guid.NewGuid()}.log");
-            File.WriteAllText(outputFilePath, exception.ToString());
+            if (exception == null)
+                throw new ArgumentNullException(nameof(exception));
+
+            string fileName = $"RPGMakerDecrypter-{DateTime.UtcNow:yyyyMMdd_HHmmss_fff}-{Guid.NewGuid()}.log";
+            string outputFilePath = Path.Combine(Path.GetTempPath(), fileName);
+
+            string exceptionDetails = exception.ToString();
+
+            File.WriteAllText(outputFilePath, exceptionDetails, Encoding.UTF8);
 
             return outputFilePath;
         }
